f969d0d846dcefbb9e68a250af91bb94e0100fa7
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-11 13:47:25 +0300

    create dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <main>
+          {" "}
+          <Dictionary />{" "}
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by{" "}
+            <a
+              href="www.linkedin.com/in/gamze-senyurek-087094147"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Gamze Senyurek
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/senyurekgamze/react-dictionary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open sourced on Github.
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
