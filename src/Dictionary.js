4714a9dcb6bca22c78dc50bc799024e7874aad9d
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-12 21:09:20 +0300

    added photos.js component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionary(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionary);
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1 `;
+    let pexelsApiKey =
+      "JsCUz3xE676HHLE414sgmx1nGnxANvbik1VjctzKaLh6J3dBdBLTQpeW";
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -46,6 +59,7 @@ export default function Dictionary(props) {
           <div className="hint">suggested words: sunset, wine, moon...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        {props.photos.map(function (photo, index) {
+          return (
+            <img
+              src={photo.src.landscape}
+              className="img-fluid"
+              alt="images"
+              key={index}
+            />
+          );
+        })}
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
