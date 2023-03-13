bc2010fda8a98318d4fb771a786d538e0399144d
senyurekgamze <121289344+senyurekgamze@users.noreply.github.com>
2023-03-13 13:35:07 +0300

    update phonetic.js and style it

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
+  let audio = new Audio(props.phonetic.audio);
+
+  const start = () => {
+    audio.play();
+  };
+
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank rel=noreferer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
+      <button className="btn  border-0" onClick={start}>
+        ▶︎ &nbsp; Listen
+      </button>
+      <span className="pronounce">({props.phonetic.text})</span>
     </div>
   );
 }
